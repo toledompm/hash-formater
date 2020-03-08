023c7e7caaee9fcd2828f940b305f1dd44e63fc3
toledompm <toledompm@gmail.com>
2020-03-08 12:44:45 -0300

    fix server.js use router

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes/router.js');
 
-
 const app = express();
-const port = '8080';
+const port = '3000';
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:false}));
 
-app.use('*',router);
+app.use('/',router);
 
 app.listen(port, () =>{
     console.log('listening on 3000');
